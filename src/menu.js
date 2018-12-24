3739feab74a40adc3d358716ac0ad331ada51b82
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-12-24 13:41:51 +1000

    Register the textButton in the GO factory and add buttons to the menu

--- src/menu.js
+++ src/menu.js
@@ -1,26 +1,43 @@
+import TextButton from './Classes/TextButton'
+
 class Menu extends Phaser.Scene {
-    constructor () {
+    constructor() {
         super({
             key: 'menu',
-            plugins: [ 'InputPlugin' ]
+            plugins: ['InputPlugin']
         });
     }
 
-    init () {
+    init() {
         console.log('menu');
+        Phaser.GameObjects.GameObjectFactory.register('textButton', function(scene, x, y, text, style, callback) {
+            let textButton = new TextButton(scene, x, y, text, style, callback);
+
+            this.displayList.add(textButton);
+            return textButton;
+        });
     }
 
-    create () {
+    create() {
+
         this.add.sprite(100, 100, 'coin').setScale(4).play('coinSpin');
         this.add.bitmapText(100, 50, 'mainFont', 'TESTING FONT');
-        this.startScene('play');
+
+        this.add.textButton(this, 400, 300, 'Play', { fontSize: '32px' }, () => {
+            console.log('Clicked Play');
+            this.startScene('play');
+        });
+        this.helpButton = this.add.textButton(this, 400, 400, 'Help', { fontSize: '32px' }, () => {
+            console.log('Clicked Help')
+        });
+
     }
 
-    startScene (key) {
+    startScene(key) {
         this.scene.start(key);
     }
 
-    update () {
+    update() {
 
     }
 }
